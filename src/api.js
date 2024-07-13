05a25d78bf12479fee6d4b2f304ae14fc4e55ed5
NaseThaBoss <emmalaehi@gmail.com>
2024-07-13 17:17:48 +0100

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "af74634e71msh5cbf558802c3645p17a66ajsn639979baeb5b",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9db63edcddfd803e3471aad3fb63231b"; 
